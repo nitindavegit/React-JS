6e0963ec3eee58b2f1986a2dacf56f8809e2bb8a
Nitin Dave <nitindave2111@gmail.com>
2026-01-13 20:47:34 +0530

    some manipulation

--- vitereact/src/Chai.jsx
+++ vitereact/src/Chai.jsx
@@ -1,6 +1,9 @@
 function Chai() {
     return (
-        <h1>I love Coffee and chai both </h1>
+        <>
+            <h1>I love Coffee and chai both </h1>
+            <h2>React is fun ! </h2>
+        </>
     )
 }
 
