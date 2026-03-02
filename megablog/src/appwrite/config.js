c5bba6bc174774f160b6b70c15e086ceace85a0e
Nitin Dave <nitindave2111@gmail.com>
2026-03-02 22:55:22 +0530

    mega project phase 1 done

---
+++ megablog/src/appwrite/config.js
@@ -0,0 +1,146 @@
+import config from "../conf/config";
+
+import { Client, Account, ID, Databases, Storage, Query } from 'appwrite'
+
+export class Service {
+    client = new Client()
+    databases;
+    bucket;
+    constructor() {
+        this.client
+            .setEndpoint(config.appWriteUrl)
+            .setProject(config.appWriteProjectId);
+
+        this.databases = new Databases(this.client);
+
+        this.bucket = new Storage(this.client)
+    }
+
+    async createPost({ title, slug, content, featuredImage, status, userId }) {
+        try {
+            return await this.databases.createDocument(
+                config.appWriteDatabaseId,
+                config.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite: create post :: error", error);
+
+        }
+    }
+
+    async updatePost(slug, { title, content, featuredImage, status }) {
+        try {
+            return await this.databases.updateDocument(
+                config.appWriteDatabaseId,
+                config.appWriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+
+        } catch (error) {
+            console.log("Appwrite service :: update post :: error", error);
+
+        }
+
+    }
+
+    async deletePost(slug) {
+        try {
+            await this.databases.deleteDocument(
+                config.appWriteDatabaseId,
+                config.appWriteCollectionId,
+                slug
+
+            )
+
+            return true
+
+        } catch (error) {
+            console.log("Appwrite service :: delete post :: error", error);
+            return false
+        }
+    }
+
+    async getPost(slug) {
+        try {
+            return await this.databases.getDocument(
+                config.appWriteDatabaseId,
+                config.appWriteCollectionId,
+                slug,
+            )
+
+        } catch (error) {
+            console.log("Appwrite: get post :: error", error);
+            return false
+        }
+
+
+    }
+
+    async getPosts(queries = [Query.equal('status', 'active')]) {
+        try {
+            return await this.databases.listDocuments(
+                config.appWriteDatabaseId,
+                config.appWriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite: get posts :: error", error);
+            return false
+        }
+    }
+
+    // file upload service
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                config.appWriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite:  upload file:: error", error);
+            return false
+
+        }
+    }
+
+    async deleteFile(fileId) {
+        try {
+            await this.bucket.deleteFile(
+                config.appWriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite:  delete file:: error", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId) {
+        return this.bucket.getFilePreview(
+            config.appWriteBucketId,
+            fileId
+        )
+    }
+}
+
+
+
+const service = new Service();
+
+export default service
---
+++ megablog/src/conf/config.js
@@ -0,0 +1,9 @@
+const config = {
+    appWriteUrl : String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+    appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appWriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appWriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID) ,
+    appWriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+}
+
+export default config
