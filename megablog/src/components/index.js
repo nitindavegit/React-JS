dfde0878fe5dd31a38ca0663929ddc002a085508
Nitin Dave <nitindave2111@gmail.com>
2026-03-06 18:44:06 +0530

    phase 2

---
+++ megablog/src/components/index.js
@@ -0,0 +1,10 @@
+import Header from './Header/Header.jsx'
+import Footer from './Footer/Footer.jsx'
+import Container from './container/Container.jsx'
+import Logo from './Logo.jsx'
+import LogoutBtn from './Header/LogOutBtn.jsx'
+
+
+export {
+    Header, Footer, Container, Logo, LogoutBtn
+}
